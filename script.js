34f9552999ddc1a8bed249a0ec5e0cf5f3e79daa
swapnil-shah <swapnilshah.me@gmail.com>
2020-12-21 16:45:28 -0500

    added to compelete, delete and store locally features

--- script.js
+++ script.js
@@ -1,25 +1,106 @@
-// 1. Select all elements
-const form = document.querySelector('#new-item-form')
+// Features
+// Add Todos
+// User will type in todo and click add add todo button. This should then add the todo to the list
+
+// Delete Todos
+// Complete Todos
+// Save Todos
+// Load Todos
+
+
+const form = document.querySelector('#new-todo-form')
+const todoInput = document.querySelector('#todo-input')
 const list = document.querySelector('#list')
-const input = document.querySelector('#item-input')
+const template = document.querySelector('#list-item-template')
+const button = document.querySelector('button')
+const LOCAL_STORAGE_PREFIX = 'ADVANCED_TODO_LIST'
+const TODOS_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}-todos`
+let todos = loadTodos()
+todos.forEach(renderTodo)
 
-// 2. Add a new element on form submit
-form.addEventListener('submit', e => {
-  e.preventDefault();
-
-  // 1.Create a new item
-  const item = document.createElement('div');
-  item.innerText = input.value
-  item.classList.add("list-item")
-
-  // 2. Add that item to the list
-  list.appendChild(item)
-
-  // 3. Clear input
-  input.value = "";
-
-  // 4. Setup the event listner to delete item when clicked
-  item.addEventListener('click', () => {
-    list.remove()
-  })
+// Complete Todos
+list.addEventListener("click", e => {
+  if (!e.target.matches('[ data-button-delete')) return
+  const parent = e.target.closest(".list-item")
+  const todoId = parent.dataset.todoId
+  parent.remove()
+  todos = todos.filter(t => t.id !== todoId)
+  saveTodos()
+  // renderTodo(newTodo)
 })
+
+// Complete Todos
+list.addEventListener("change", e => {
+  if (!e.target.matches('[ data-list-item-checkbox')) return
+  // Get the todo which is checked
+  const parent = e.target.closest(".list-item")
+  const todoId = parent.dataset.todoId
+  const todo = todos.find(t => t.id == todoId)
+  // Toggle the complete property to be equal to checkbox value
+  todo.complete = e.target.checked
+  // Save our updated todo
+  saveTodos()
+})
+
+// Features
+// Add Todos
+// User will type in todo and click add add todo button. This should then add the todo to the list
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  const todoName = todoInput.value
+  if (todoName == '') return
+  const newTodo = { //object creation is required to handle completed todos upon refresh
+    name: todoName,
+    complete: false,
+    id: new Date().valueOf().toString() //Id is used to keep track of completed todos. Very helpful when mmultiple todos are same
+  }
+  renderTodo(newTodo)
+  todos.push(newTodo)
+  saveTodos()
+  todoInput.value = ''
+})
+
+function renderTodo(todo) {
+  const templateClone = template.content.cloneNode(true)
+  const listItem = templateClone.querySelector('.list-item')
+  listItem.dataset.todoId = todo.id
+  const textElement = templateClone.querySelector('[data-list-item-text]')
+  textElement.innerText = todo.name
+  const checkbox = templateClone.querySelector('[data-list-item-checkbox]')
+  checkbox.checked = todo.complete
+  list.appendChild(templateClone)
+}
+
+// Load Todos
+function loadTodos() {
+  const todosString = localStorage.getItem(TODOS_STORAGE_KEY)
+  return JSON.parse(todosString) || []//Short circuit to [] if local storage is cleared
+}
+// Save Todos
+function saveTodos() {
+  localStorage.setItem(TODOS_STORAGE_KEY, JSON.stringify(todos))
+}
+
+
+
+
+// Modal JS
+// const modal = document.querySelector("#modal")
+// const openModalBtn = document.querySelector("#open-modal-btn")
+// const closeModalBtn = document.querySelector("#close-modal-btn")
+// const overlay = document.querySelector("#overlay")
+// const modalClasses = [modal, overlay];
+// const showModal = () => {
+//   modalClasses.forEach(modalClass => {
+//     modalClass.classList.add("open");
+//   })
+// }
+// const closeModal = () => {
+//   modalClasses.forEach(modalClass => {
+//     modalClass.classList.remove("open");
+//   })
+// }
+// openModalBtn.addEventListener('click', showModal)
+// closeModalBtn.addEventListener('click', closeModal)
+// overlay.addEventListener('click', closeModal)
